2931c32d60751ad3a56b35e2b8ea8e65ce6347e0
roopa7sri <153494632+roopa7sri@users.noreply.github.com>
2025-07-25 08:42:00 +0530

    commnets update

--- block_stickynotes_form.php
+++ block_stickynotes_form.php
@@ -4,24 +4,25 @@ defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
 require_once($CFG->libdir . '/formslib.php');
-
+// Define the form class for stickynotes by extending moodleform
 class block_stickynotes_form extends moodleform {
     public function definition() {
         $mform = $this->_form;
+        // Retrieve 'editing' data passed to the form (used to pre-fill values when editing)
         $editing = $this->_customdata['editing'] ?? null;
-
+ // Hidden field for note ID (used during editing)
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_INT);
         $mform->setDefault('id', $editing->id ?? 0);
-
+ // Textarea for the note content
         $mform->addElement('textarea', 'note', get_string('note', 'block_stickynotes'), 'wrap="virtual" rows="5" cols="40"');
         $mform->setType('note', PARAM_TEXT);
         $mform->addRule('note', null, 'required', null, 'client');
         $mform->setDefault('note', $editing->note ?? '');
-
+// Date and time selector for due date
         $mform->addElement('date_time_selector', 'duedate', get_string('duedate', 'block_stickynotes'));
         $mform->setDefault('duedate', $editing->duedate ?? time());
-
+// Submit button
         $mform->addElement('submit', 'submitbutton', get_string('savenote', 'block_stickynotes'));
     }
 }
